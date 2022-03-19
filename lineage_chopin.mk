Xiaomi-Chopin-Device/device_xiaomi_chopin-early

chopin: Initial tree for chopin
Co-authored-by: kubersharma001 <kubersharma001@gmail.com>
Co-authored-by: palaych <palaych@arrowos.net>
Co-authored-by: Lineu Martins <lineumartins001@gmail.com>

Change-Id: I64aad6415c327957ebed713e75230864eb3d2066
Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>
Signed-off-by: LynnrinChan <wuxiaofei31@gmail.com>

lineage_chopin.mk
@@ -1,0 +1,25 @@
+#
+# Copyright (C) 2018-2021 ArrowOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit common products
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configurations
+$(call inherit-product, device/xiaomi/chopin/device.mk)
+
+# Inherit common LineageOS configurations
+$(call inherit-product, vendor/lineage/config/common.mk)
+
+PRODUCT_CHARACTERISTICS := nosdcard
+
+PRODUCT_NAME := lineage_chopin
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := chopin
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := M2104K10AC
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

Lynnrin-Studio/android_device_xiaomi_chopin

lineage_chopin.mk
@@ -1,0 +1,25 @@
+#
+# Copyright (C) 2018-2021 ArrowOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit common products
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configurations
+$(call inherit-product, device/xiaomi/chopin/device.mk)
+
+# Inherit common LineageOS configurations
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_CHARACTERISTICS := nosdcard
+
+PRODUCT_NAME := lineage_chopin
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := chopin
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := M2104K10AC
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi