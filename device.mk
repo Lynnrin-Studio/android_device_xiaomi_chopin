2ddfba6438e430827c1031c092a1068d5fd6b230
LynnrinChan <wuxiaofei31@gmail.com>
2022-03-19 20:25:09 +0800

    chopin: Import audio policy configuration
    Signed-off-by: LynnrinChan <wuxiaofei31@gmail.com>

--- device.mk
+++ device.mk
@@ -31,6 +31,10 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml::$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_configuration.xml
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
